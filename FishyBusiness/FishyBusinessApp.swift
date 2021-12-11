ef85917ad34594fda59f19bc711ea8020bdc793b
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-11 10:18:05 -0500

    Initial Commit

---
+++ FishyBusiness/FishyBusinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishyBusinessApp.swift
+//  FishyBusiness
+//
+//  Created by Madison Dellamea on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FishyBusinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
